f5f2f5e4373f736bf86a7d3ea5df61d76b491b31
Gavin Sharma <GAVINSHARMA06@GMAIL.COM>
2026-08-26 23:35:16 -0300

    java file set up

---
+++ src/test/java/dev/miniredis/MiniRedisStore.java
@@ -0,0 +1,7 @@
+package dev.miniredis;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MiniRedisStore {
+    private Map<String,String> redisStorage = new HashMap<String, String>();
+}
