53c8aceea3c3aa08dea5dcf94a85f9643fe8a890
Gavin Sharma <GAVINSHARMA06@GMAIL.COM>
2026-08-28 19:39:29 -0300

    added contains method and created relevant Junit test cases

--- src/main/java/dev/miniredis/MiniRedisStore.java
+++ src/main/java/dev/miniredis/MiniRedisStore.java
@@ -5,11 +5,17 @@ import java.util.Map;
 public class MiniRedisStore {
     private Map<String,String> redisStorage = new HashMap<>();
 
-    public void set(String key, String value){
+    public void set(String key, String value) {
         redisStorage.put(key, value);
     }
 
-    public String get(String key){
+    public String get(String key) {
         return redisStorage.get(key);
     }
+
+    public boolean exists(String key) {
+        return redisStorage.containsKey(key);
+    }
+
+
 }
--- src/test/java/dev/miniredis/MiniRedisStoreTest.java
+++ src/test/java/dev/miniredis/MiniRedisStoreTest.java
@@ -1,16 +1,31 @@
 package dev.miniredis;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class MiniRedisStoreTest {
 
     @Test
-    void setValueThenGetStoredValue(){
+    void setValueReturnsStoredValue(){
         MiniRedisStore testStore = new MiniRedisStore();
         testStore.set("language","Java");
         String actual = testStore.get("language");
         assertEquals("Java",actual);
+    }
+
+    @Test
+    void presentKeyShouldExist(){
+        MiniRedisStore testStore = new MiniRedisStore();
+        testStore.set("David","exists");
+        assertTrue(testStore.exists("David"));
+    }
+
+    @Test
+    void missingKeyShouldNotExist(){
+        MiniRedisStore testStore = new MiniRedisStore();
+        testStore.set("Alice","exists");
+        assertFalse(testStore.exists("Ali"));
     }
 }
